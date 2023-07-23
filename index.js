1f79dc5a9b1e74b49180ac71ca251dac2102c6c6
HasibulHasan <hasibulhasan372@gmail.com>
2023-07-23 20:15:56 +0600

    users gget api created

--- index.js
+++ index.js
@@ -54,6 +54,10 @@ async function run() {
             const result = await candidateCollection.insertOne(candidate);
             res.send(result)
     });
+    app.get("/users", async(req, res) =>{
+        const result = await userCollection.find().toArray();
+            res.send(result)
+    })
     app.post("/users", async(req, res) =>{
         const user = req.body;
         const query = { email: user.email }
